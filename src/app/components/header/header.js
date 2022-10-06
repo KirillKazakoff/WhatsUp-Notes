323a7a5670323b17288756628fe1a2a809ff3fd4
keka3avr <kekazavr228@gmail.com>
2022-10-07 00:53:56 +1100

    search help btn was added

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -4,6 +4,7 @@ import info from '../info/info';
 
 export default class Header {
     constructor(uploadH, downloadOnFilterH, cancelFilterH, setScrollBlockH) {
+        this.isFirstSearch = true;
         this.isFilter = false;
         this.cancelFilterH = cancelFilterH;
         this.setScrollBlockH = setScrollBlockH;
@@ -11,6 +12,7 @@ export default class Header {
         this.container = document.querySelector('.header');
         this.pinBtn = this.container.querySelector('.header-badge__pin');
         this.pinInput = this.container.querySelector('.input__file');
+        this.help = this.container.querySelector('.header-badge__help');
 
         this.search = this.container.querySelector('.header-badge__search');
         this.searchInput = this.container.querySelector('.input__search');
@@ -20,12 +22,15 @@ export default class Header {
         this.search.addEventListener('click', (e) => this.onSearch(e));
         this.searchInput.addEventListener('input', (e) => this.onInput(e));
         this.pinBtn.addEventListener('click', (e) => this.upload.onClick(e));
+        this.help.addEventListener('click', () => info.renderInfo(info.messages.search.title, info.messages.search.desc));
     }
 
     onSearch() {
-        if (!this.isFilter) {
+        if (this.isFirstSearch) {
             info.renderInfo(info.messages.search.title, info.messages.search.desc);
+            this.isFirstSearch = false;
         }
+
         this.setScrollBlockH();
         this.isFilter = !this.isFilter;
         this.searchInput.classList.toggle('hidden');
